79d58f433967d30f683563a56aa497959cb4bfed
Johannes Bonk <joh4nn3sbonk@gmail.com>
2019-12-21 21:10:50 +0100

    added README and LICENSE file

---
+++ WebTools/README.md
@@ -0,0 +1,15 @@
+# pdfCrawler.py
+## Description
+This is a simple tool which allows you to download and merge all PDF files from a website of your desire e.g free eBooks only available in chapters at publisher websites ...
+## Usage
+### Prerequisites
+You will need python3 installed on your system.
+### Step 1:
+Put pdfCrawler.py into the directory the **.pdf** file should be stored and start the tool with
+`python3 pdfCrawler.py`
+### Step 2:
+Enter the name for the final PDF file (keep in mind: don´t add the **.pdf** filename extension, the tool will do that for you).
+`Please insert file name(without tag): yourFileName`
+### Step 3:
+Enter the url containing the PDF you want to download and merge.
+`Input url: www.your-dowload-link.tld/PATH`
--- WebTools/src/pdfCrawler.py
+++ WebTools/src/pdfCrawler.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) 2019 Johannes Bonk
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from PyPDF2 import PdfFileMerger
 from urllib.parse import urlparse
 from urllib.request import urlopen
@@ -22,29 +44,29 @@ def getURLs():
 
 def getRndFileNames(url_list):
 	rnd_names = []
-	for k in range(len(url_list)): 
+	for k in range(len(url_list)):
 		rnd_names.append((''.join(random.choice(string.ascii_letters) for i in range(20)) + ".pdf")) # creates 20 char long random file names with .pdf tag
 	return rnd_names
 
-def generatePDFs(url_list, files): 
-	for i in range(len(url_list)): 
+def generatePDFs(url_list, files):
+	for i in range(len(url_list)):
 		response = urlopen(url_list[i]) # open connection to pdf link
 		file = open(files[i], 'wb') # generate file with name from files
 		file.write(response.read()) # write content from connection to generated file
 		file.close() # close file input stream
-	return 
+	return
 
 def mergePDFs(res_file_name, files):
 	merger = PdfFileMerger(False) # create pdf-merger object (strict = False to correct zero object error)
-	for file in files: 
-		merger.append(file) # append all generated files 
-	merger.write(res_file_name + ".pdf") # write merged files as pdf with user specific file name 
+	for file in files:
+		merger.append(file) # append all generated files
+	merger.write(res_file_name + ".pdf") # write merged files as pdf with user specific file name
 	merger.close() # close pdf-merger object
-	return 
+	return
 
-def deletePDFs(files): 
-	for file in files: 
-		os.remove(file) # remove all  downloaded single files (merged one remains) 
+def deletePDFs(files):
+	for file in files:
+		os.remove(file) # remove all  downloaded single files (merged one remains)
 	return
 
 res_file_name = input("Please insert file name(without tag): ") # user input name for result file
